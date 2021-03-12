ff960d134ad70606fa8d584aff81f5474e6c7f13
arithy <software@arithy.org>
2021-03-13 05:54:09 +0900

    Fix the return type of main() in tests/main.c

--- tests/main.c
+++ tests/main.c
@@ -6,9 +6,10 @@
 
 #include "parser.c"
 
-void main(void) {
+int main(int argc, char **argv) {
     int ret;
     pcc_context_t *ctx = pcc_create(NULL);
     while (pcc_parse(ctx, &ret));
     pcc_destroy(ctx);
+    return 0;
 }
