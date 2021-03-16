d2f02decd643592f4c7288316703be3c25f6e2da
arithy <software@arithy.org>
2021-03-17 06:48:31 +0900

    Support UNICODE escape sequences
    Surrogate pairs are also available.

--- src/packcc.c
+++ src/packcc.c
@@ -452,29 +452,107 @@ static bool_t unescape_string(char *str) {
             case 't': str[j++] = '\x09'; break;
             case 'v': str[j++] = '\x0b'; break;
             case 'x':
-                if (str[i + 1] == '\0') {
-                    str[j++] = '\\'; str[j++] = 'x'; str[j] = '\0'; return FALSE;
-                }
-                if (str[i + 2] == '\0') {
-                    str[j++] = '\\'; str[j++] = 'x'; str[j++] = str[i + 1]; str[j] = '\0'; return FALSE;
-                }
                 {
-                    char c = str[i + 1];
-                    char d = str[i + 2];
-                    c = (c >= '0' && c <= '9') ? c - '0' :
-                        (c >= 'a' && c <= 'f') ? c - 'a' + 10 :
-                        (c >= 'A' && c <= 'F') ? c - 'A' + 10 : -1;
-                    d = (d >= '0' && d <= '9') ? d - '0' :
-                        (d >= 'a' && d <= 'f') ? d - 'a' + 10 :
-                        (d >= 'A' && d <= 'F') ? d - 'A' + 10 : -1;
-                    if (c < 0 || d < 0) {
-                        str[j++] = '\\'; str[j++] = 'x'; str[j++] = str[i + 1]; str[j++] = str[i + 2];
+                    char s = 0, c;
+                    size_t k;
+                    for (k = 0; k < 2; k++) {
+                        c = str[i + k + 1];
+                        const char d =
+                            (c >= '0' && c <= '9') ? c - '0' :
+                            (c >= 'a' && c <= 'f') ? c - 'a' + 10 :
+                            (c >= 'A' && c <= 'F') ? c - 'A' + 10 : -1;
+                        if (d < 0) break;
+                        s = (s << 4) | d;
+                    }
+                    if (k < 2) {
+                        const size_t l = i + k;
+                        str[j++] = '\\'; str[j++] = 'x';
+                        while (i <= l) str[j++] = str[++i];
+                        if (c == '\0') return FALSE;
                         b = FALSE;
+                        continue;
                     }
-                    else {
-                        str[j++] = (c << 4) | d;
-                    }
+                    str[j++] = s;
                     i += 2;
+                }
+                break;
+            case 'u':
+                {
+                    int s = 0, t = 0;
+                    char c;
+                    size_t k;
+                    for (k = 0; k < 4; k++) {
+                        c = str[i + k + 1];
+                        const char d =
+                            (c >= '0' && c <= '9') ? c - '0' :
+                            (c >= 'a' && c <= 'f') ? c - 'a' + 10 :
+                            (c >= 'A' && c <= 'F') ? c - 'A' + 10 : -1;
+                        if (d < 0) break;
+                        s = (s << 4) | d;
+                    }
+                    if (k < 4 || (s & 0xfc00) == 0xdc00) { /* invalid character or invalid surrogate code point */
+                        const size_t l = i + k;
+                        str[j++] = '\\'; str[j++] = 'u';
+                        while (i <= l) str[j++] = str[++i];
+                        if (c == '\0') return FALSE;
+                        b = FALSE;
+                        continue;
+                    }
+                    if ((s & 0xfc00) == 0xd800) { /* surrogate pair */
+                        for (k = 4; k < 10; k++) {
+                            c = str[i + k + 1];
+                            if (k == 4) {
+                                if (c != '\\') break;
+                            }
+                            else if (k == 5) {
+                                if (c != 'u') break;
+                            }
+                            else {
+                                const char d =
+                                    (c >= '0' && c <= '9') ? c - '0' :
+                                    (c >= 'a' && c <= 'f') ? c - 'a' + 10 :
+                                    (c >= 'A' && c <= 'F') ? c - 'A' + 10 : -1;
+                                if (d < 0) break;
+                                t = (t << 4) | d;
+                            }
+                        }
+                        if (k < 10 || (t & 0xfc00) != 0xdc00) { /* invalid character or invalid surrogate code point */
+                            const size_t l = i + 4; /* NOTE: Not i + k to redo with recovery. */
+                            str[j++] = '\\'; str[j++] = 'u';
+                            while (i <= l) str[j++] = str[++i];
+                            b = FALSE;
+                            continue;
+                        }
+                    }
+                    {
+                        const int u = t ? ((((s & 0x03ff) + 0x0040) << 10) | (t & 0x03ff)) : s;
+                        if (u < 0x0080) {
+                            str[j++] = (char)u;
+                        }
+                        else if (u < 0x0800) {
+                            str[j++] = (char)(0xc0 | (u >> 6));
+                            str[j++] = (char)(0x80 | (u & 0x3f));
+                        }
+                        else if (u < 0x010000) {
+                            str[j++] = (char)(0xe0 | (u >> 12));
+                            str[j++] = (char)(0x80 | ((u >> 6) & 0x3f));
+                            str[j++] = (char)(0x80 | (u & 0x3f));
+                        }
+                        else if (u < 0x110000) {
+                            str[j++] = (char)(0xf0 | (u >> 18));
+                            str[j++] = (char)(0x80 | ((u >> 12) & 0x3f));
+                            str[j++] = (char)(0x80 | ((u >>  6) & 0x3f));
+                            str[j++] = (char)(0x80 | (u & 0x3f));
+                        }
+                        else { /* never reached theoretically; in case */
+                            const size_t l = i + 10;
+                            str[j++] = '\\'; str[j++] = 'u';
+                            while (i <= l) str[j++] = str[++i];
+                            b = FALSE;
+                            continue;
+                        }
+                    }
+                    i += t ? 10 : 4;
                 }
                 break;
             case '\n': break;
